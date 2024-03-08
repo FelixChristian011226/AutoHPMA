1065b32880a6c80e8c40c7aa2baed2bd52f6548e
wangyuefei <1930552052@qq.com>
2024-03-08 16:06:49 +0800

    Update. Changed the method of screen capture, to implent at backgroud.

--- AutoHPMA/Helpers/ScreenCaptureHelper.cs
+++ AutoHPMA/Helpers/ScreenCaptureHelper.cs
@@ -10,15 +10,59 @@ namespace AutoHPMA.Helpers
         [DllImport("user32.dll")]
         public static extern bool GetWindowRect(IntPtr hWnd, out Rectangle lpRect);
 
+        [DllImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+
+        [DllImport("gdi32.dll", SetLastError = true)]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteDC(IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hdc, IntPtr hObject);
+
         public static Bitmap CaptureWindow(IntPtr hWnd)
         {
-            GetWindowRect(hWnd, out Rectangle rect);
+            // 获取目标窗口的设备上下文
+            IntPtr windowDC = GetWindowDC(hWnd);
+            // 创建兼容的内存设备上下文
+            IntPtr memDC = CreateCompatibleDC(windowDC);
 
-            Bitmap bmp = new Bitmap(rect.Width, rect.Height, PixelFormat.Format32bppArgb);
-            using (Graphics graphics = Graphics.FromImage(bmp))
-            {
-                graphics.CopyFromScreen(rect.Left, rect.Top, 0, 0, rect.Size, CopyPixelOperation.SourceCopy);
-            }
+            GetWindowRect(hWnd, out Rectangle rect);
+            int width = rect.Width - rect.X;
+            int height = rect.Height - rect.Y;
+
+            // 创建兼容的位图
+            IntPtr hBitmap = CreateCompatibleBitmap(windowDC, width, height);
+
+            // 选择位图对象到内存DC中，并保留旧的位图
+            IntPtr oldBitmap = SelectObject(memDC, hBitmap);
+
+            // 将目标窗口内容打印到内存DC中的位图上
+            PrintWindow(hWnd, memDC, 0);
+
+            // 从内存DC中获取位图信息
+            Bitmap bmp = Bitmap.FromHbitmap(hBitmap);
+
+            // 清理
+            SelectObject(memDC, oldBitmap);
+            DeleteObject(hBitmap);
+            DeleteDC(memDC);
+            DeleteObject(windowDC);
+
             return bmp;
         }
 
