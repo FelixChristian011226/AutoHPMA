08159476e8e0deb959ad536bf84b95e87771c641
FelixChristian011226 <1930552052@qq.com>
2025-03-13 14:43:53 +0800

    feat: add match template function for recognition.

---
+++ AutoHPMA/Helpers/MatchTemplateHelper.cs
@@ -0,0 +1,51 @@
+﻿using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoHPMA.Helpers
+{
+    public class MatchTemplateHelper
+    {
+        public static OpenCvSharp.Point MatchTemplate(Mat srcMat, Mat dstMat, TemplateMatchModes matchMode, Mat? maskMat = null, double threshold = 0.8)
+        {
+            try
+            {
+                using var result = new Mat();
+                Cv2.MatchTemplate(srcMat, dstMat, result, matchMode, maskMat!);
+
+                if (matchMode is TemplateMatchModes.SqDiff or TemplateMatchModes.CCoeff or TemplateMatchModes.CCorr)
+                {
+                    Cv2.Normalize(result, result, 0, 1, NormTypes.MinMax);
+                }
+
+                Cv2.MinMaxLoc(result, out var minValue, out var maxValue, out var minLoc, out var maxLoc);
+
+                if (matchMode is TemplateMatchModes.SqDiff or TemplateMatchModes.SqDiffNormed)
+                {
+                    if (minValue <= 1 - threshold)
+                    {
+                        return minLoc;
+                    }
+                }
+                else
+                {
+                    if (maxValue >= threshold)
+                    {
+                        return maxLoc;
+                    }
+                }
+
+                return default;
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError(ex.Message);
+                //_logger.LogDebug(ex, ex.Message);
+                return default;
+            }
+        }
+    }
+}
