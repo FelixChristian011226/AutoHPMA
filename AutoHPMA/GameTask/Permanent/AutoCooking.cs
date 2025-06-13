fa76419b6d0f631d79baa43119445f46562abfb3
FelixChristian011226 <1930552052@qq.com>
2025-06-13 16:31:16 +0800

    feat: add ingredients locate function in auto cooking.

--- AutoHPMA/GameTask/Permanent/AutoCooking.cs
+++ AutoHPMA/GameTask/Permanent/AutoCooking.cs
@@ -1,5 +1,6 @@
 using AutoHPMA.Helpers;
 using AutoHPMA.Helpers.CaptureHelper;
+using AutoHPMA.Helpers.ImageHelper;
 using AutoHPMA.Helpers.RecognizeHelper;
 using AutoHPMA.Services;
 using AutoHPMA.Views.Windows;
@@ -36,11 +37,17 @@ public class AutoCooking : IGameTask
     private AutoCookingState _state = AutoCookingState.Outside;
 
     private Mat? captureMat;
+    private Mat oven, pot;
+    private Mat rice, fish;
 
     private List<Rect> detect_rects = new List<Rect>();
 
+    private bool initialized = false;
     private int _autoCookingTimes;
     private int round = 0;
+
+    private Rect oven_rect, pot_rect;
+    private Rect rice_rect, fish_rect;
 
     private CancellationTokenSource _cts;
     public event EventHandler? TaskCompleted;
@@ -73,7 +80,11 @@ public class AutoCooking : IGameTask
             {
                 System.GC.Collect();
                 await Task.Delay(500, _cts.Token);
-                _logger.LogInformation("自动烹饪进行中");
+                if(!initialized)
+                {
+                    Initialize();
+                }
+                
             }
         }
         catch (TaskCanceledException)
@@ -93,8 +104,98 @@ public class AutoCooking : IGameTask
         }
     }
 
+    private bool Initialize()
+    {
+        if (!LocateKitchenWare())
+        {
+            //_logger.LogDebug("未定位到厨房用具，即将开始重新定位。");
+            return false;
+        }
+        else
+        {
+            _maskWindow?.SetLayerRects("Kitchenware", new List<Rect>{ ScaleRect(oven_rect, scale), ScaleRect(pot_rect, scale) });
+        }
+        if(!LocateIngredients())
+        {
+            //_logger.LogDebug("未定位到食材，即将开始重新定位。");
+            return false;
+        }
+        else
+        {
+            _maskWindow?.SetLayerRects("Ingredients", new List<Rect>{ ScaleRect(rice_rect, scale), ScaleRect(fish_rect, scale) });
+        }
+
+        _logger.LogInformation("自动烹饪初始化完成。");
+        initialized = true;
+        return true;
+    }
+
     private bool LocateKitchenWare()
     {
+        double threshold = 0.9;
+        captureMat = _capture.Capture();
+        Cv2.Resize(captureMat, captureMat, new Size(captureMat.Width / scale, captureMat.Height / scale));
+        Cv2.CvtColor(captureMat, captureMat, ColorConversionCodes.BGRA2BGR);
+        Mat maskMat;
+        using (var ovenBGR = oven.Clone())
+        {
+            Cv2.CvtColor(ovenBGR, ovenBGR, ColorConversionCodes.BGRA2BGR);
+            maskMat = MatchTemplateHelper.GenerateMask(oven);
+            var matchpoint = MatchTemplateHelper.MatchTemplate(captureMat, ovenBGR, TemplateMatchModes.CCoeffNormed, maskMat, threshold);
+            if(matchpoint == default)
+            {
+                //_logger.LogDebug("未定位到烤箱，即将重试。");
+                return false;
+            }
+            oven_rect = new Rect(matchpoint.X, matchpoint.Y, oven.Width, oven.Height);
+        }
+
+        using (var potBGR = pot.Clone())
+        {
+            Cv2.CvtColor(potBGR, potBGR, ColorConversionCodes.BGRA2BGR);
+            maskMat = MatchTemplateHelper.GenerateMask(pot);
+            var matchpoint = MatchTemplateHelper.MatchTemplate(captureMat, potBGR, TemplateMatchModes.CCoeffNormed, maskMat, threshold);
+            if(matchpoint == default) 
+            {
+                //_logger.LogDebug("未定位到锅，即将重试。");
+                return false;
+            }
+            pot_rect = new Rect(matchpoint.X, matchpoint.Y, pot.Width, pot.Height);
+        }
+        return true;
+    }
+
+    private bool LocateIngredients()
+    {
+        double threshold = 0.9;
+        captureMat = _capture.Capture();
+        Cv2.Resize(captureMat, captureMat, new Size(captureMat.Width / scale, captureMat.Height / scale));
+        Cv2.CvtColor(captureMat, captureMat, ColorConversionCodes.BGRA2BGR);
+        Mat maskMat;
+        using (var riceBGR = rice.Clone())
+        {
+            Cv2.CvtColor(riceBGR, riceBGR, ColorConversionCodes.BGRA2BGR);
+            maskMat = MatchTemplateHelper.GenerateMask(rice);
+            var matchpoint = MatchTemplateHelper.MatchTemplate(captureMat, riceBGR, TemplateMatchModes.SqDiffNormed, maskMat, threshold);
+            if(matchpoint == default)
+            {
+                //_logger.LogDebug("未定位到米饭，即将重试。");
+                return false;
+            }
+            rice_rect = new Rect(matchpoint.X, matchpoint.Y, rice.Width, rice.Height);
+        }
+        using (var fishBGR = fish.Clone())
+        {
+            Cv2.CvtColor(fishBGR, fishBGR, ColorConversionCodes.BGRA2BGR);
+            maskMat = MatchTemplateHelper.GenerateMask(fish);
+            var matchpoint = MatchTemplateHelper.MatchTemplate(captureMat, fishBGR, TemplateMatchModes.SqDiffNormed, maskMat, threshold);
+            if(matchpoint == default)
+            {
+                //_logger.LogDebug("未定位到鱼，即将重试。");
+                return false;
+            }
+            fish_rect = new Rect(matchpoint.X, matchpoint.Y, fish.Width, fish.Height);
+        }
         return true;
     }
 
@@ -102,11 +203,21 @@ public class AutoCooking : IGameTask
     {
         _maskWindow.AddLayer("Match");
         _maskWindow.AddLayer("Kitchenware");
+        _maskWindow.AddLayer("Condiments");
+        _maskWindow.AddLayer("Ingredients");
     }
 
     public void LoadAssets()
     {
         string image_folder = "Assets/Cooking/Image/";
+        //Kitchenware
+        oven = Cv2.ImRead(image_folder + "Kitchenware/oven.png", ImreadModes.Unchanged);
+        pot = Cv2.ImRead(image_folder + "Kitchenware/pot.png", ImreadModes.Unchanged);
+        //Condiments
+
+        //Ingredients
+        rice = Cv2.ImRead(image_folder + "Ingredients/rice.png", ImreadModes.Unchanged);
+        fish = Cv2.ImRead(image_folder + "Ingredients/fish.png", ImreadModes.Unchanged);
 
     }
 
@@ -119,6 +230,15 @@ public class AutoCooking : IGameTask
         offsetX = left - leftMumu;
         offsetY = top - topMumu;
         scale = width / 1280.0;
+    }
+    private Rect ScaleRect(Rect rect, double scale)
+    {
+        return new Rect(
+            (int)(rect.X * scale),
+            (int)(rect.Y * scale),
+            (int)(rect.Width * scale),
+            (int)(rect.Height * scale)
+        );
     }
 
     #region SetParameter
